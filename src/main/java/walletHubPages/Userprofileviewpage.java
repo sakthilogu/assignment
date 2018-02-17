82605c30922374ace36ee94c218e9cfdac85d0de
sivasakthi <sakthi.irtt@gmail.com>
2018-02-17 23:07:25 +0530

    Update Userprofileviewpage.java

--- src/main/java/walletHubPages/Userprofileviewpage.java
+++ src/main/java/walletHubPages/Userprofileviewpage.java
@@ -11,9 +11,6 @@ public class Userprofileviewpage extends Welcomepage {
 	public Userprofileviewpage openingurl() throws FileNotFoundException, IOException  {
 		loadObjects();
 		driver.navigate().to("https://wallethub.com/profile/test_insurance_company/");
-		//Thread.sleep(800);
-		
-		
 		return this;
 	}
 	
@@ -31,21 +28,7 @@ public class Userprofileviewpage extends Welcomepage {
 		Thread.sleep(800);
 		enterbyXPath(prop.getProperty("email.add"), prop.getProperty("email.value"));
 		enterbyXPath(prop.getProperty("password"), prop.getProperty("pwd.value"));
-		clickByXpath(prop.getProperty("login.button3"));
-		
-		//String profile_user=driver.findElement(By.xpath(prop.getProperty("username.xpath"))).getText();
-		//String actual_user=prop.getProperty("username");
-//		if(profile_user.equalsIgnoreCase(actual_user))
-//		{
-//			System.out.println(profile_user +"Logged in successfully");
-//		}
-//		else
-//		{
-//			System.out.println(profile_user +"failed to login");
-//		}
-		
-		
-		
+		clickByXpath(prop.getProperty("login.button3"));		
 		return this;
 	}
 	public WriteReviewpage clickonFourthStar() throws FileNotFoundException, IOException, InterruptedException {
@@ -65,39 +48,13 @@ public class Userprofileviewpage extends Welcomepage {
 		mousehoverByxpath(prop.getProperty("star3.xpath"));
 		mousehoverByxpath(prop.getProperty("star4.xpath"));
 		mousehoverByxpath(prop.getProperty("star5.xpath"));
-		clickbyActions(prop.getProperty("star5.xpath"));
-
-		//WebElement element1 = driver.findElement(By.xpath(prop.getProperty("enable.review")));
-
-//		String strJavaScript = "var element = arguments[0];"
-//				+ "var mouseEventObj = document.createEvent('MouseEvents');"
-//				+ "mouseEventObj.initEvent( 'mouseover', true, true );" + "element.dispatchEvent(mouseEventObj);";
-//		JavascriptExecutor executor = (JavascriptExecutor) driver;
-//		executor.executeScript(strJavaScript, element1);
-		// WebElement element =
-		// driver.findElement(By.xpath(prop.getProperty("star3.xpath")));
-		// executor1.executeScript(strJavaScript, element);
-//		Thread.sleep(800);
-//		WebElement firstStar = driver.findElement(By.xpath("//*[@id='wh-body-inner']/div[2]/div[3]/div[1]/div/a[2]"));
-//		executor.executeScript(strJavaScript, firstStar);
-//		Thread.sleep(800);
-//		WebElement twoStar = driver.findElement(By.xpath("//*[@id='wh-body-inner']/div[2]/div[3]/div[1]/div/a[3]"));
-//		executor.executeScript(strJavaScript, twoStar);
-//		Thread.sleep(800);
-//		WebElement thirdStar = driver.findElement(By.xpath("//*[@id='wh-body-inner']/div[2]/div[3]/div[1]/div/a[4]"));
-//		executor.executeScript(strJavaScript, thirdStar);
-//		Thread.sleep(1000);
-//		WebElement fourthStar = driver.findElement(By.xpath("//*[@id='wh-body-inner']/div[2]/div[3]/div[1]/div/a[5]"));
-//		executor.executeScript(strJavaScript, fourthStar);
-//		Thread.sleep(1000);
-		//executor.executeScript("arguments[0].click();", fourthStar);
+		clickbyActions(prop.getProperty("star5.xpath"));		
 		return new WriteReviewpage();
 	}
 
 	public WriteReviewpage clickonlink() {
 		driver.navigate().back();
 		clickByXpath(prop.getProperty("review.button"));
-
 		return new WriteReviewpage();
 	}
 }
